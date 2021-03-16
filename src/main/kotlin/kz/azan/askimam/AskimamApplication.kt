174c757eecbb132e504cd2b282373b03d1ffbca4
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-16 21:09:03 +0600

    Init

---
+++ src/main/kotlin/kz/azan/askimam/AskimamApplication.kt
@@ -0,0 +1,11 @@
+package kz.azan.askimam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AskimamApplication
+
+fun main(args: Array<String>) {
+	runApplication<AskimamApplication>(*args)
+}
---
+++ src/test/kotlin/kz/azan/askimam/AskimamApplicationTests.kt
@@ -0,0 +1,13 @@
+package kz.azan.askimam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AskimamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
