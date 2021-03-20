b928965713d844282499964ab94ee456c6811e99
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-20 17:17:20 +0600

    Add failing message additions

--- src/test/kotlin/kz/azan/askimam/chat/domain/model/ChatPolicyTest.kt
+++ src/test/kotlin/kz/azan/askimam/chat/domain/model/ChatPolicyTest.kt
@@ -1,17 +1,37 @@
 package kz.azan.askimam.chat.domain.model
 
+import kz.azan.askimam.chat.domain.policy.AddMessagePolicy
+import kz.azan.askimam.common.type.NotBlankString
+import kz.azan.askimam.user.domain.model.User
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class ChatPolicyTest : ChatFixtures() {
 
     @Test
-    internal fun `should add a new message`() {
+    internal fun `should not add a new message`() {
         fixtureClockAndThen(30)
 
         fixtureChat().run {
-//            addTextMessageByInquirer(fixtureMessageId, fixtureNewMessage)
+            val option = addTextMessage(
+                AddMessagePolicy.forInquirer,
+                fixtureMessageId,
+                fixtureNewMessage,
+                User(User.Id(10), User.Type.Inquirer),
+            )
 
+            assertThat(option.isDefined).isTrue
         }
     }
 
+    @Test
+    internal fun `should not add a new audio`() {
+        val audio = NotBlankString.of("Аудио")
+        fixtureClock()
+        fixtureChat(audio).run {
+            val option = addAudioMessage(AddMessagePolicy.forImam, Message.Id(2), fixtureAudio, fixtureInquirer)
+
+            assertThat(option.isDefined).isTrue
+        }
+    }
 }
