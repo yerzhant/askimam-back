1eb001f271078e8c702a631d823d59fd5f6f9134
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-22 18:11:27 +0600

    Move delete policy to policy package

--- src/main/kotlin/kz/azan/askimam/favorite/domain/policy/DeleteFavoritePolicy.kt
+++ src/main/kotlin/kz/azan/askimam/favorite/domain/policy/DeleteFavoritePolicy.kt
@@ -1,15 +1,16 @@
-package kz.azan.askimam.favorite.domain.model
+package kz.azan.askimam.favorite.domain.policy
 
 import io.vavr.control.Option
 import io.vavr.kotlin.none
 import io.vavr.kotlin.some
 import kz.azan.askimam.common.domain.Declination
+import kz.azan.askimam.favorite.domain.model.Favorite
 import kz.azan.askimam.user.domain.model.User
 
 fun interface DeleteFavoritePolicy {
     fun isAllowed(favorite: Favorite, user: User): Option<Declination>
 
-    companion object{
+    companion object {
         val forAll = DeleteFavoritePolicy { favorite, user ->
             if (favorite.userId == user.id)
                 none()
--- src/test/kotlin/kz/azan/askimam/favorite/domain/policy/DeleteFavoritePolicyTest.kt
+++ src/test/kotlin/kz/azan/askimam/favorite/domain/policy/DeleteFavoritePolicyTest.kt
@@ -1,9 +1,10 @@
-package kz.azan.askimam.favorite.domain.model
+package kz.azan.askimam.favorite.domain.policy
 
 import io.vavr.kotlin.some
 import kz.azan.askimam.chat.domain.model.ChatFixtures
 import kz.azan.askimam.common.domain.Declination
-import kz.azan.askimam.favorite.domain.model.DeleteFavoritePolicy.Companion.forAll
+import kz.azan.askimam.favorite.domain.model.Favorite
+import kz.azan.askimam.favorite.domain.policy.DeleteFavoritePolicy.Companion.forAll
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
