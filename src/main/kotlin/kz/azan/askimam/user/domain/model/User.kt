606c9e43cab852fea287136bf781db22fd34923f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-22 17:17:51 +0600

    Add name to a user

--- src/main/kotlin/kz/azan/askimam/user/domain/model/User.kt
+++ src/main/kotlin/kz/azan/askimam/user/domain/model/User.kt
@@ -1,8 +1,11 @@
 package kz.azan.askimam.user.domain.model
 
-class User(
+import kz.azan.askimam.common.type.NonBlankString
+
+data class User(
     val id: Id,
     val type: Type,
+    val name: NonBlankString,
 ) {
     data class Id(val value: Long)
 
