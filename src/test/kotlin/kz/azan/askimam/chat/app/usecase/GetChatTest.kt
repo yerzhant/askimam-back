88413d1c7570fb75dcd8281663aedef62d255c37
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-24 00:22:56 +0600

    Remove repos from Chat

--- src/test/kotlin/kz/azan/askimam/chat/app/usecase/GetChatTest.kt
+++ src/test/kotlin/kz/azan/askimam/chat/app/usecase/GetChatTest.kt
@@ -3,6 +3,7 @@ package kz.azan.askimam.chat.app.usecase
 import io.mockk.every
 import io.vavr.kotlin.left
 import io.vavr.kotlin.right
+import kz.azan.askimam.chat.domain.model.Chat
 import kz.azan.askimam.chat.domain.model.Chat.Type.Private
 import kz.azan.askimam.chat.domain.model.ChatFixtures
 import kz.azan.askimam.common.domain.Declination
@@ -13,18 +14,16 @@ internal class GetChatTest : ChatFixtures() {
 
     @Test
     internal fun `should return a chat`() {
-        val chat = fixtureSavedChat()
+        val chat = fixtures()
         every { getCurrentUser() } returns fixtureInquirer
-        every { chatRepository.findById(fixtureChatId) } returns right(chat)
 
         assertThat(GetChat(getCurrentUser, chatRepository)(fixtureChatId).get()).isEqualTo(chat)
     }
 
     @Test
     internal fun `should return a chat to an imam`() {
-        val chat = fixtureSavedChat()
+        val chat = fixtures()
         every { getCurrentUser() } returns fixtureImam
-        every { chatRepository.findById(fixtureChatId) } returns right(chat)
 
         assertThat(GetChat(getCurrentUser, chatRepository)(fixtureChatId).get()).isEqualTo(chat)
     }
@@ -40,20 +39,27 @@ internal class GetChatTest : ChatFixtures() {
 
     @Test
     internal fun `should return a publicly visible chat`() {
-        val chat = fixtureSavedChat()
+        val chat = fixtures()
         every { getCurrentUser() } returns fixtureAnotherInquirer
-        every { chatRepository.findById(fixtureChatId) } returns right(chat)
 
         assertThat(GetChat(getCurrentUser, chatRepository)(fixtureChatId).get()).isEqualTo(chat)
     }
 
     @Test
     internal fun `should deny access to a private chat`() {
+        fixtureClock()
         val chat = fixtureSavedChat(Private)
         val declination = Declination.withReason("The operation is not permitted")
         every { getCurrentUser() } returns fixtureAnotherInquirer
         every { chatRepository.findById(fixtureChatId) } returns right(chat)
 
         assertThat(GetChat(getCurrentUser, chatRepository)(fixtureChatId).left).isEqualTo(declination)
+    }
+
+    private fun fixtures(): Chat {
+        fixtureClock()
+        val chat = fixtureSavedChat()
+        every { chatRepository.findById(fixtureChatId) } returns right(chat)
+        return chat
     }
 }
