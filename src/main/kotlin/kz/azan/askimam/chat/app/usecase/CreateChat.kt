c08354726d7347bda9b4ed66d299647ccd04918b
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-23 23:57:04 +0600

    Remove repos from Chat

--- src/main/kotlin/kz/azan/askimam/chat/app/usecase/CreateChat.kt
+++ src/main/kotlin/kz/azan/askimam/chat/app/usecase/CreateChat.kt
@@ -1,15 +1,10 @@
 package kz.azan.askimam.chat.app.usecase
 
-import io.vavr.control.Option
-import io.vavr.kotlin.none
-import io.vavr.kotlin.some
-import kz.azan.askimam.chat.domain.service.GetCurrentUser
 import kz.azan.askimam.chat.domain.model.Chat
 import kz.azan.askimam.chat.domain.model.Chat.Type
 import kz.azan.askimam.chat.domain.model.ChatRepository
-import kz.azan.askimam.chat.domain.model.MessageRepository
 import kz.azan.askimam.chat.domain.model.Subject
-import kz.azan.askimam.common.domain.Declination
+import kz.azan.askimam.chat.domain.service.GetCurrentUser
 import kz.azan.askimam.common.domain.EventPublisher
 import kz.azan.askimam.common.type.NonBlankString
 import java.time.Clock
@@ -19,39 +14,25 @@ class CreateChat(
     private val eventPublisher: EventPublisher,
     private val getCurrentUser: GetCurrentUser,
     private val chatRepository: ChatRepository,
-    private val messageRepository: MessageRepository,
 ) {
-    operator fun invoke(
-        type: Type,
-        text: NonBlankString,
-    ): Option<Declination> = Chat.new(
-        clock,
-        eventPublisher,
-        getCurrentUser,
-        chatRepository,
-        messageRepository,
-        type,
-        text,
-    ).fold(
-        { some(it) },
-        { none() }
+    operator fun invoke(type: Type, text: NonBlankString) = chatRepository.create(
+        Chat.new(
+            clock,
+            eventPublisher,
+            getCurrentUser,
+            type,
+            text,
+        )
     )
 
-    fun withSubject(
-        type: Type,
-        subject: Subject,
-        text: NonBlankString,
-    ): Option<Declination> = Chat.newWithSubject(
-        clock,
-        eventPublisher,
-        getCurrentUser,
-        chatRepository,
-        messageRepository,
-        type,
-        subject,
-        text,
-    ).fold(
-        { some(it) },
-        { none() }
+    fun withSubject(type: Type, subject: Subject, text: NonBlankString) = chatRepository.create(
+        Chat.newWithSubject(
+            clock,
+            eventPublisher,
+            getCurrentUser,
+            type,
+            subject,
+            text,
+        )
     )
 }
--- src/test/kotlin/kz/azan/askimam/chat/app/usecase/CreateChatTest.kt
+++ src/test/kotlin/kz/azan/askimam/chat/app/usecase/CreateChatTest.kt
@@ -1,10 +1,13 @@
 package kz.azan.askimam.chat.app.usecase
 
 import io.mockk.every
+import io.mockk.verifySequence
 import io.vavr.kotlin.none
-import io.vavr.kotlin.right
+import io.vavr.kotlin.some
+import kz.azan.askimam.chat.domain.event.ChatCreated
 import kz.azan.askimam.chat.domain.model.Chat.Type.Public
 import kz.azan.askimam.chat.domain.model.ChatFixtures
+import kz.azan.askimam.common.domain.Declination
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -12,33 +15,49 @@ internal class CreateChatTest : ChatFixtures() {
 
     @Test
     internal fun `should create a new chat`() {
-        fixtureClock()
-        every { getCurrentUser() } returns fixtureInquirer
-        every { chatRepository.generateId() } returns right(fixtureChatId)
-        every { messageRepository.generateId() } returns right(fixtureMessageId1)
-        every { chatRepository.create(any()) } returns none()
-        every { messageRepository.add(any()) } returns none()
-        every { eventPublisher.publish(any()) } returns Unit
+        fixtures()
 
         assertThat(
-            CreateChat(clock, eventPublisher, getCurrentUser, chatRepository, messageRepository)
+            CreateChat(clock, eventPublisher, getCurrentUser, chatRepository)
                 (Public, fixtureMessage).isEmpty
+        ).isTrue
+
+        verifySequence {
+            eventPublisher.publish(ChatCreated(null, fixtureMessage))
+            chatRepository.create(any())
+        }
+    }
+
+    @Test
+    internal fun `should not create a new chat`() {
+        fixtures()
+        every { chatRepository.create(any()) } returns some(Declination.withReason("hi"))
+
+        assertThat(
+            CreateChat(clock, eventPublisher, getCurrentUser, chatRepository)
+                (Public, fixtureMessage).isDefined
         ).isTrue
     }
 
     @Test
     internal fun `should create a new chat with a subject`() {
-        fixtureClock()
-        every { getCurrentUser() } returns fixtureInquirer
-        every { chatRepository.generateId() } returns right(fixtureChatId)
-        every { messageRepository.generateId() } returns right(fixtureMessageId1)
-        every { chatRepository.create(any()) } returns none()
-        every { messageRepository.add(any()) } returns none()
-        every { eventPublisher.publish(any()) } returns Unit
+        fixtures()
 
         assertThat(
-            CreateChat(clock, eventPublisher, getCurrentUser, chatRepository, messageRepository)
+            CreateChat(clock, eventPublisher, getCurrentUser, chatRepository)
                 .withSubject(Public, fixtureSubject, fixtureMessage).isEmpty
         ).isTrue
+
+        verifySequence {
+            eventPublisher.publish(ChatCreated(fixtureSubject, fixtureMessage))
+            chatRepository.create(any())
+        }
+    }
+
+    private fun fixtures() {
+        fixtureClock()
+        every { getCurrentUser() } returns fixtureInquirer
+        every { chatRepository.create(any()) } returns none()
+        every { eventPublisher.publish(any()) } returns Unit
     }
 }
