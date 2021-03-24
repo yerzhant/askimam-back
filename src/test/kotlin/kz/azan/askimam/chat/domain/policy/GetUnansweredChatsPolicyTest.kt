0d7d89a03e2c7f697095b431708ab759042d04d6
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-24 20:15:34 +0600

    Add GetUnansweredChatsPolicy

---
+++ src/test/kotlin/kz/azan/askimam/chat/domain/policy/GetUnansweredChatsPolicyTest.kt
@@ -0,0 +1,18 @@
+package kz.azan.askimam.chat.domain.policy
+
+import kz.azan.askimam.chat.domain.model.ChatFixtures
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class GetUnansweredChatsPolicyTest : ChatFixtures() {
+
+    @Test
+    internal fun `should allow to view`() {
+        assertThat(GetUnansweredChatsPolicy.forAll.isAllowed(fixtureImam).isEmpty).isTrue
+    }
+
+    @Test
+    internal fun `should not allow to view`() {
+        assertThat(GetUnansweredChatsPolicy.forAll.isAllowed(fixtureInquirer).isDefined).isTrue
+    }
+}
