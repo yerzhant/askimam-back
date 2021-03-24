6db7169321eca14808c4b10d09fad6b23919f576
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-24 14:52:36 +0600

    Fix chats.sql

--- src/test/resources/scripts/chats.sql
+++ src/test/resources/scripts/chats.sql
@@ -1,3 +1,4 @@
+-- *************** 1 *****************
 insert into chats(
     id,
     asked_by,
@@ -84,4 +85,51 @@ insert into messages(
     null
 );
 
---insert into chats values(2);
+-- *************** 2 *****************
+insert into chats(
+    id,
+    asked_by,
+    answered_by,
+    type,
+    is_visible_to_public,
+    updated_at,
+    inquirer_fcm_token,
+    is_viewed_by_imam,
+    is_viewed_by_inquirer,
+    created_at,
+    imam_fcm_token,
+    subject
+) values(
+    2,
+    2,
+    null,
+    'Public',
+    false,
+    now(),
+    '456',
+    true,
+    true,
+    now(),
+    null,
+    'Subject'
+);
+
+insert into messages(
+    id,
+    chat_id,
+    author_id,
+    created_at,
+    type,
+    text,
+    audio,
+    updated_at
+) values (
+    4,
+    2,
+    2,
+    now(),
+    'Text',
+    'A message',
+    null,
+    null
+);
