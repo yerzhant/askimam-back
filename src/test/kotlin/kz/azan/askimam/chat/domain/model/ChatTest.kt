178f831d8d6e69b609912c9077ba325f6927e1f9
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-24 19:59:34 +0600

    Fix chat tests

--- src/test/kotlin/kz/azan/askimam/chat/domain/model/ChatTest.kt
+++ src/test/kotlin/kz/azan/askimam/chat/domain/model/ChatTest.kt
@@ -71,7 +71,7 @@ internal class ChatTest : ChatFixtures() {
             getCurrentUser = getCurrentUser,
         ).run {
             assertThat(subject()).isNull()
-            assertThat(subjectText()).isEqualTo(fixtureMessage)
+            assertThat(subjectText()).isEqualTo(Subject(fixtureMessage))
         }
 
         verify {
