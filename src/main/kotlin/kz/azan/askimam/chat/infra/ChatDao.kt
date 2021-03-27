338d7100c83eb8519aa61723789165ab6d5c295f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-28 00:51:39 +0600

    Make chat dao transactionally read only

--- src/main/kotlin/kz/azan/askimam/chat/infra/ChatDao.kt
+++ src/main/kotlin/kz/azan/askimam/chat/infra/ChatDao.kt
@@ -3,7 +3,9 @@ package kz.azan.askimam.chat.infra
 import kz.azan.askimam.chat.domain.model.Chat
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional(readOnly = true)
 interface ChatDao : CrudRepository<ChatRow, Long> {
     fun findByTypeAndIsVisibleToPublicIsTrue(type: Chat.Type, pageable: Pageable): List<ChatRow>
     fun findByAnsweredByOrderByUpdatedAtDesc(userId: Long, pageable: Pageable): List<ChatRow>
