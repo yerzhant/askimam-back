ae299842eab4f484ee92a3aa4d4a125a91b875fc
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-28 13:02:42 +0600

    Fix web tests

--- src/test/kotlin/kz/azan/askimam/meta/ControllerTest.kt
+++ src/test/kotlin/kz/azan/askimam/meta/ControllerTest.kt
@@ -1,6 +1,8 @@
 package kz.azan.askimam.meta
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import kz.azan.askimam.common.web.security.JwtService
 import kz.azan.askimam.favorite.FavoriteFixtures
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.TestConstructor
@@ -15,4 +17,7 @@ open class ControllerTest : FavoriteFixtures() {
 
     @Autowired
     protected lateinit var objectMapper: ObjectMapper
+
+    @MockkBean
+    protected lateinit var jwtService: JwtService
 }
