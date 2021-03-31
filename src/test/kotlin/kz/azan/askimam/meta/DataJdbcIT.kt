57571d3e1c0b46c36be5aa5c2458c0d086d04b7c
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-31 22:02:04 +0600

    Beautify

--- src/test/kotlin/kz/azan/askimam/meta/DataJdbcIT.kt
+++ src/test/kotlin/kz/azan/askimam/meta/DataJdbcIT.kt
@@ -2,10 +2,13 @@ package kz.azan.askimam.meta
 
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE
 import org.springframework.test.context.TestConstructor
+import org.springframework.test.context.TestConstructor.AutowireMode.ALL
+import kotlin.annotation.AnnotationTarget.CLASS
 
-@Target(AnnotationTarget.CLASS)
+@Target(CLASS)
 @DataJdbcTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@AutoConfigureTestDatabase(replace = NONE)
+@TestConstructor(autowireMode = ALL)
 annotation class DataJdbcIT
