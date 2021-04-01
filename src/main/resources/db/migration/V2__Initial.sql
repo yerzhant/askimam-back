63c190d1c6a4f1cc2eb3143f70ecc3bca30ba67f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-01 19:35:52 +0600

    Add duration to messages table

--- src/main/resources/db/migration/V2__Initial.sql
+++ src/main/resources/db/migration/V2__Initial.sql
@@ -12,8 +12,8 @@ create table chats(
     imam_fcm_token          varchar(255),
     subject                 varchar(255),
 
-    key fk_chats_asked_by           (asked_by),
-    key fk_chats_answered_by        (answered_by),
+    key        ix_chats_asked_by                (asked_by),
+    key        ix_chats_answered_by             (answered_by),
     constraint fk_chats_asked_by    foreign key (asked_by)      references users(id),
     constraint fk_chats_answered_by foreign key (answered_by)   references users(id)
 );
@@ -26,11 +26,12 @@ create table messages(
     type                    varchar(100)    not null,
     author_type             varchar(100)    not null,
     text                    text            not null,
-    audio                   varchar(1024),
     updated_at              datetime,
+    duration                varchar(100),   -- TODO: add duration to Message entity
+    audio                   varchar(1024),
 
-    key fk_messages_chat_id             (chat_id),
-    key fk_messages_author_id           (author_id),
+    key        ix_messages_chat_id                  (chat_id),
+    key        ix_messages_author_id                (author_id),
     constraint fk_messages_chat_id      foreign key (chat_id)   references chats(id),
     constraint fk_messages_author_id    foreign key (author_id) references users(id)
 );
@@ -41,8 +42,8 @@ create table favorites(
     chat_id     bigint          not null,
     added_at    datetime        not null,
 
-    unique key uk_favorites_user_id_chat_id (user_id, chat_id),
-    key fk_favorites_chat_id            (chat_id),
+    unique key uk_favorites_user_id_chat_id         (user_id, chat_id),
+    key        ix_favorites_chat_id                 (chat_id),
     constraint fk_favorites_user_id     foreign key (user_id)   references users(id),
     constraint fk_favorites_chat_id     foreign key (chat_id)   references chats(id) on delete cascade
 );
--- src/main/resources/db/migration/V3__Firebase collections.sql
+++ src/main/resources/db/migration/V3__Firebase collections.sql
@@ -1,7 +1,6 @@
 create table fb_topics(
     id                   varchar(255)        not null primary key,
     uid                  varchar(255)        not null,
-    is_linked_to_user    bit                 not null default false,
     is_public            bit                 not null,
     name                 varchar(1000)       not null,
     created_on           datetime            not null,
@@ -12,41 +11,43 @@ create table fb_topics(
     imam_uid             varchar(255),
     is_answered          bit,
     imam_fcm_token       varchar(255),
-    imam_viewed_on       datetime,
-
-    index ix_fb_topics_uid (uid)
+    imam_viewed_on       datetime
 );
 
 create table fb_messages(
     id                  varchar(255)            not null primary key,
     uid                 varchar(255)            not null,
-    is_linked_to_user   bit                     not null default false,
     topic_id            varchar(255)            not null,
     created_on          datetime                not null,
     sender              char                    not null,
     text                text                    not null,
     message_id          bigint,
     sender_name         varchar(255),
-    audio_url           varchar(1000),
     edited_on           datetime,
-
-    key ix_fb_messages_uid (uid)
+    duration            varchar(100),
+    audio_url           varchar(1000)
 );
 
 create table fb_favorites(
     id                  varchar(255)            not null primary key,
     uid                 varchar(255)            not null,
-    is_linked_to_user   bit                     not null default false,
     topic_id            varchar(255)            not null,
     created_on          datetime                not null,
     topic_name          varchar(1000)           not null,
-    favorite_id         bigint,
-
-    key ix_fb_favorites_uid (uid)
+    favorite_id         bigint
 );
 
 create table fb_imams(
     id                  varchar(255)            not null primary key,
     user_id             int                     not null,
-    name                varchar(255)            not null
+    name                varchar(255)            not null,
+
+    unique key uk_fb_imams_name (name)
+);
+
+create table fb_profiles(
+    id                  varchar(255)            not null primary key,
+    login               varchar(1000)           not null,
+    timezone            int                     not null,
+    user_id             int
 );
