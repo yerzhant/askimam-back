96896f47b40dc56c34793a8b9176cf93eb70bd96
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-02 19:42:22 +0600

    Remove uid from fb_profiles table

--- src/main/resources/db/migration/V3__Firebase collections.sql
+++ src/main/resources/db/migration/V3__Firebase collections.sql
@@ -45,7 +45,6 @@ create table fb_imams(
 
 create table fb_profiles(
     id                  varchar(255)            not null primary key,
-    uid                 varchar(255)            not null,
     login               varchar(1000)           not null,
     timezone            int                     not null,
     user_id             int
