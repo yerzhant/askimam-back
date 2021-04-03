8cb028ad018ed3d742157429fabf6d5d1964a162
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-03 13:17:59 +0600

    Add clean up script

---
+++ dev/drop-tables.sql
@@ -0,0 +1,9 @@
+drop table fb_favorites;
+drop table fb_messages;
+drop table fb_topics;
+drop table fb_imams;
+drop table fb_profiles;
+drop table favorites;
+drop table messages;
+drop table chats;
+drop table flyway_schema_history;
