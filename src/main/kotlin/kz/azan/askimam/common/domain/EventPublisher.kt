886f43dd82811dac693bd53ee06ac5ccec5d3e67
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-20 20:35:02 +0600

    Add todo

--- src/main/kotlin/kz/azan/askimam/common/domain/EventPublisher.kt
+++ src/main/kotlin/kz/azan/askimam/common/domain/EventPublisher.kt
@@ -1,5 +1,6 @@
 package kz.azan.askimam.common.domain
 
+// TODO: send notification not to imam's topic, but to all (or some) of them by their token ids
 interface EventPublisher {
     fun publish(event: Event)
 }
