ca6d174c7560b31a4a85a9876c1c28aed52df1ff
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-05-03 07:12:45 +0600

    Fix fcm tokens table

--- src/main/resources/db/migration/V4__FCM tokens.sql
+++ src/main/resources/db/migration/V4__FCM tokens.sql
@@ -1,5 +1,5 @@
 create table fcm_tokens(
-    value       varchar(1024)           not null primary key,
+    value       varchar(768)            not null primary key,
     user_id     int                     not null,
 
     key        ix_fcm_tokens_user_id                (user_id),
