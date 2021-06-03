50c863c84f523ed1341d63144e7db7ee424007e3
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-06-04 00:47:00 +0600

    v1.5

--- src/test/kotlin/kz/azan/askimam/meta/ControllerTest.kt
+++ src/test/kotlin/kz/azan/askimam/meta/ControllerTest.kt
@@ -6,11 +6,14 @@ import kz.azan.askimam.favorite.FavoriteFixtures
 import kz.azan.askimam.security.service.JwtService
 import kz.azan.askimam.security.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.context.TestConstructor.AutowireMode.ALL
 import org.springframework.test.web.servlet.MockMvc
 
 @TestConstructor(autowireMode = ALL)
+@Import(MyRestTemplateBuilder::class)
 open class ControllerTest : FavoriteFixtures() {
 
     @Autowired
@@ -25,3 +28,5 @@ open class ControllerTest : FavoriteFixtures() {
     @MockkBean
     protected lateinit var userService: UserService
 }
+
+class MyRestTemplateBuilder : RestTemplateBuilder()
--- src/test/kotlin/kz/azan/askimam/meta/DataJdbcIT.kt
+++ src/test/kotlin/kz/azan/askimam/meta/DataJdbcIT.kt
@@ -3,6 +3,7 @@ package kz.azan.askimam.meta
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.context.TestConstructor.AutowireMode.ALL
 import kotlin.annotation.AnnotationTarget.CLASS
@@ -11,4 +12,5 @@ import kotlin.annotation.AnnotationTarget.CLASS
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = NONE)
 @TestConstructor(autowireMode = ALL)
+@Import(MyRestTemplateBuilder::class)
 annotation class DataJdbcIT
