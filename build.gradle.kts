73c54983bb686af7b422e6fb37b4cbba7ed5471b
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2022-02-05 01:16:32 +0600

    v1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ val registry = System.getenv("AskimamRegistry") ?: "localhost:5000"
 jib {
     from {
         @Suppress("SpellCheckingInspection")
-        image = "$registry/openjdk:11-slim@sha256:b789d521bbe81ab0991c59c6d604cf5bec6a2257128a0ecafb15b2d63bbce872"
+        image = "$registry/openjdk:11-slim@sha256:3f19e6e5394a6f601af082cfc4491eaf9b5cd85c6f576c84a7c47aef07542ba8"
     }
     to {
         image = "$registry/askimam-back"
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export version=1.6.0
+export version=1.6.1
 export spring_profiles_active=mysql
 export GOOGLE_APPLICATION_CREDENTIALS=google/service-account.json
 ./gradlew build
