39387f21941f2520ea9b9d245384a7b341c8f70f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2024-01-23 22:23:31 +0600

    Upgrade sb+

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,28 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-    dependencies {
-        classpath("com.google.cloud.tools:jib-spring-boot-extension-gradle:0.1.0")
-    }
-}
-
 plugins {
-    id("org.springframework.boot") version "2.4.3"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("com.google.cloud.tools.jib") version "3.1.2"
-    kotlin("jvm") version "1.4.30"
-    kotlin("plugin.spring") version "1.4.30"
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "kz.azan"
 version = System.getenv("version")
-java.sourceCompatibility = JavaVersion.VERSION_11
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
 }
-
-extra["testcontainersVersion"] = "1.15.3"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
@@ -31,69 +31,42 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.vavr:vavr-kotlin:0.10.2")
-    implementation("com.auth0:java-jwt:3.14.0")
-    implementation("com.google.firebase:firebase-admin:9.2.0")
-
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    runtimeOnly("mysql:mysql-connector-java")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(module = "junit")
-        exclude(module = "mockito-core")
-    }
+//    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("com.tngtech.archunit:archunit-junit5:0.17.0")
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
     testRuntimeOnly("com.h2database:h2")
 }
 
-dependencyManagement {
-    imports {
-        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
-    }
+dependencies {
+    implementation(platform("io.arrow-kt:arrow-stack:1.2.1"))
+    implementation("io.arrow-kt:arrow-core")
+//    implementation("io.vavr:vavr-kotlin:0.10.4")
+    implementation("com.auth0:java-jwt:4.4.0")
+    implementation("com.google.firebase:firebase-admin:9.2.0")
+
+//    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+//        exclude(module = "junit")
+//        exclude(module = "mockito-core")
+//    }
+    testImplementation("com.tngtech.archunit:archunit-junit5:1.2.1")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-    failFast = true
-}
-
-tasks {
-    build {
-        dependsOn(jib)
-    }
-}
-
-val registry = System.getenv("AskimamRegistry") ?: "localhost:5000"
-
-jib {
-    from {
-        image = "$registry/openjdk:11-slim@sha256:3f19e6e5394a6f601af082cfc4491eaf9b5cd85c6f576c84a7c47aef07542ba8"
-    }
-    to {
-        image = "$registry/askimam-back"
-        tags = setOf("$version")
-    }
-    container {
-        user = "azan"
-        creationTime = "USE_CURRENT_TIMESTAMP"
-    }
-    pluginExtensions {
-        pluginExtension {
-            implementation = "com.google.cloud.tools.jib.gradle.extension.springboot.JibSpringBootExtension"
-        }
-    }
 }
