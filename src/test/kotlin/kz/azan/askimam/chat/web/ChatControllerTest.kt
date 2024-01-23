f845f3985bc28d44718b5976c32b6a50547fd597
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2024-01-23 23:51:25 +0600

    FIx ChatControllerTest

--- src/test/kotlin/kz/azan/askimam/chat/web/ChatControllerTest.kt
+++ src/test/kotlin/kz/azan/askimam/chat/web/ChatControllerTest.kt
@@ -3,9 +3,12 @@ package kz.azan.askimam.chat.web
 import com.ninjasquad.springmockk.MockkBean
 import kz.azan.askimam.chat.app.usecase.*
 import kz.azan.askimam.meta.ControllerTest
+import kz.azan.askimam.security.web.config.WebSecurityConfig
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.Import
 
 @WebMvcTest(ChatController::class)
+@Import(WebSecurityConfig::class)
 internal open class ChatControllerTest : ControllerTest() {
 
     protected val url = "/chats"
